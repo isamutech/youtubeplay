4412254dde5b2ce54c5b2e61795591294488cd73
isamutech <32417898+isamutech@users.noreply.github.com>
2018-07-26 10:59:13 -0400

    Initial commit

---
+++ app/src/main/java/com/samudra/admin/youtubeplay/MainActivity.java
@@ -0,0 +1,21 @@
+package com.samudra.admin.youtubeplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.loadUrl("https://www.youtube.com/embed/1vkJQ69_MTE");
+
+    }
+}
